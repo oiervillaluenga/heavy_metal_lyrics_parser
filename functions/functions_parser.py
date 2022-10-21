dfb90cf3ba9d9f4d7bda0bedcb58cf558c33d5d2
oiervillaluenga <115699916+oiervillaluenga@users.noreply.github.com>
2022-10-21 07:01:51 +0200

    data granularity increased
    Changes made to get more granularity in the data, from capturing data monthly to capturing data in a 7 day interval

--- functions/functions_parser.py
+++ functions/functions_parser.py
@@ -5,6 +5,8 @@ import yaml
 from logging.handlers import TimedRotatingFileHandler
 import os
 import itertools
+import matplotlib.pyplot as plt
+import matplotlib
 
 current_dir = os.getcwd()
 
@@ -74,7 +76,76 @@ def create_year_range(year):
     >>>convertir_fechas(2022-01-01)
     2022-12-31 00:00:00
     '''
-    months = range(1,13) 
+    months = range(1,13)
+    days = [1,7,13,19,25] 
     year_list = [year]   
-    combined_list = list(itertools.product(year_list, months))
+    combined_list = list(itertools.product(year_list, months,days))
     return combined_list
+
+def create_month_range(year,month):
+    '''Creates a time range combining month and year for a period between start and end years
+    :return: list
+    >>>convertir_fechas(2022-01-01)
+    2022-12-31 00:00:00
+    '''
+    month = [month]
+    days = [1,7,13,19,25] 
+    year = [year]   
+    combined_list = list(itertools.product(year, month,days))
+    return combined_list
+
+def density_function_plot_n1_songs(path,header,data):
+    """Create a plot using the header and the data dataframes for n1 songs
+    :param path: the path to where the plot will be saved
+    :param data: a dataframe that contains all measurement points for all characteristics
+    :param header: a dataframe that contains key information for the characteristic that we need to filter the datapoints for   
+    >>> density_function_plot(path,header,data) 
+    
+    """      
+    # We set the characteristics of the plot
+    gridsize = (4,4)
+    fig = plt.figure(figsize = (12.8,9.6))
+    plt.Figure(dpi=500)
+    # We create 2 axes: ax1 for the top part where it will hold the graph and ax3 for the information table
+    ax1 = plt.subplot2grid(gridsize,(0,0),colspan = 4, rowspan = 3)
+    ax3 = plt.subplot2grid(gridsize, (3, 0),colspan = 4, rowspan = 1)
+        
+    # We define ax1 as the current axe on the plot
+    plt.sca(ax1)        
+        
+    # we plot a kde graph based on a dataframe
+    data['DeltaTimeN1'].plot.kde(title = f'Prob (PDF) and Cum (CDF) Density Functions of n1 songs',ax=ax1)
+    print('kde graph ok')
+    # we define the axises
+    ax1.set_xlabel("Weeks_n1_songs", fontsize = 16)
+    ax1.set_ylabel("PDF Frecuency",color="blue",fontsize=16)
+        
+    # We create a twin object for two different y-axis on the sample plot
+    ax2=ax1.twinx()
+        
+    # We plot the cdf (cumulative distribution function)
+    ax2.plot(data['DeltaTimeN1'], data['cdf'],color="green")
+    print('cdf graph ok')
+    # We name the Y axis
+    ax2.set_ylabel("% of measurements",color="red",fontsize=16)
+        
+    # We set ax3 as the current axes 
+    plt.sca(ax3)
+    # We hide all the axises and borders
+    ax3.get_xaxis().set_visible(False)
+    ax3.get_yaxis().set_visible(False)
+    plt.box(on=None)
+    
+    summary_table = header
+    # We use the dataframe to create a table
+    table = plt.table(cellText=summary_table,colLabels=summary_table.columns,cellLoc = 'center', rowLoc = 'center',loc='center',colWidths=[0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15])
+        
+    # We set auto set font size to false to speed up the canvas drawing process
+    table.auto_set_font_size(False)    
+        
+    # We increase the scale of the row height
+    table.scale(1, 2)
+    print('just before save fig ok')
+    # We save and close the figure
+    plt.savefig(f"{path}/plt_kde_N1_songs_cdf_plot.jpg")
+    plt.close()
--- lyrics_parser.py
+++ lyrics_parser.py
@@ -9,6 +9,7 @@ import itertools
 import os
 import tqdm
 import glob
+import numpy as np
 
 # We get the current directory 
 current_dir = os.getcwd()
@@ -38,11 +39,13 @@ except Exception as e:
     exit
 
 # I check if there already exists a parquet file in the working directory where the data is stored 
-if os.path.isfile(all_data_file) == False:
+while os.path.isfile(all_data_file) == False:
     #If there isn't we proceed to the data extraction
     try:
-        list_years  = [fp.create_year_range(x) for x in range(1970,2020)]
-        name_years  = [x for x in range(1970,2020)]
+        list_years  = [x for x in range(1970,2020)]
+        list_months = [x for x in range(1,13)]
+        list_years_months = list(itertools.product(list_years, list_months))
+        data_extraction_dates  = [fp.create_month_range(x,y) for x,y in list_years_months]
     except Exception as e: 
         print('Failure when trying to create the list of months and years...')
         print(e)
@@ -51,23 +54,29 @@ if os.path.isfile(all_data_file) == False:
         time.sleep(30)
         exit    
 
-    # we iterate through each year in the list of years to extract the results of the billboard songs and we append each df to the dictionary
-    for year,name in zip(list_years,name_years):
+    # we iterate through each year_month combination in the list of years to extract the results of the billboard songs and we append each df to the dictionary
+    for date,name in zip(data_extraction_dates,list_years_months):
         # we create an empty dictionary where we will store the dataframes with the billboard song results for each decade
-        data_file = current_dir + '\\data' + f'{name}.parquet'
+        data_file = current_dir + '\\data' + f'{name[0]}_{name[1]}.parquet'
         if os.path.isfile(data_file) == False:
             df_collection = {}
             start = time.time()
-            for year,month in year:
-                print(f'year:{year} and month: {month} started')
-                url = f'http://www.umdmusic.com/default.asp?Lang=English&Chart=D&ChDay=28&ChMonth={month}&ChYear={year}&ChBand=&ChSong='
-                df_url = pd.read_html(url)[9]
+            for year, month, day in date:
+                print(f'year:{year} month: {month} day: {day} started')
+                url = f'http://www.umdmusic.com/default.asp?Lang=English&Chart=D&ChDay={day}&ChMonth={month}&ChYear={year}&ChBand=&ChSong='
+                try: 
+                    df_url = pd.read_html(url)[9]
+                except Exception as e:
+                    print('Error when reading url in pandas')
+                    print(e)
+                    continue
                 df_url = df_url.iloc[2:,:]
-                parsing_date = f'{year}-{month}-28'
+                parsing_date = f'{year}-{month}-{day}'
                 dt = datetime.datetime.strptime(parsing_date,'%Y-%m-%d')
                 df_url['date'] = dt
                 result = {parsing_date:df_url}
                 df_collection[parsing_date] = df_url
+                del df_url
 
             end = time.time()
             loop_exec_time = end - start
@@ -111,9 +120,9 @@ if os.path.isfile(all_data_file) == False:
             loop_exec_time = end - start
             logger.debug(f'exec time for loop to transform songs data for {name}: {loop_exec_time}')
             df_results.to_parquet(data_file)
-            print(f'year: {name} completed')
-        else:    
-            print('Data for this year already exists')
+            print(f'year: {name[0]} month: {name[1]} completed')
+            del df_results,df_contents_artists,df_contents_songs,df_contents
+            
 
     # we use glob to combine all parquet files into one file in case that a one unique file doesnt exist
 
@@ -127,11 +136,62 @@ if os.path.isfile(all_data_file) == False:
 else:
     print('overall agreggated result file already exists')  
 
-# continue here with the analysis    
-print('tralala')  
+#ANALYSIS: CHARACTERIZATION OF N1 SONGS
+# We will ge the average time of a n1 song at n1 
+results = pd.read_parquet(all_data_file)
+results['ThisWeekPos'] = results['ThisWeekPos'].fillna(0).astype(int)
+results['ThisWeekPeakPos'] = results['ThisWeekPeakPos'].fillna(0).astype(int)
+results['ThisWeekTotalWeeks'] = results['ThisWeekTotalWeeks'].fillna(0).astype(int)
+results['OverallPeakPos'] = results['OverallPeakPos'].fillna(0).astype(int)
+results['OverallTotalWeeks'] = results['OverallTotalWeeks'].fillna(0).astype(int)
 
+# ¿How long do songs stay in n1?
+# We filter the df for n1 songs
+type = results.dtypes
+results_n1 = results.loc[results['ThisWeekPos'] == 1].copy()
 
+# We get the first and last dates for each song at n1
+min_date_n1 = results_n1.groupby(['Artist','Title'])[['ParsingDate']].min()
+max_date_n1 = results_n1.groupby(['Artist','Title'])[['ParsingDate']].max()
 
+min_date_n1 = min_date_n1.rename(columns={'ParsingDate':'FirstDateN1'})
+max_date_n1 = max_date_n1.rename(columns={'ParsingDate':'LastDateN1'})
+
+# We first merge the min_date results back to results_n1
+results_n1 = results_n1.merge(min_date_n1,how='left',left_on=['Artist','Title'], right_on = ['Artist','Title'],suffixes = ('', '_DROP'))
+results_n1 = results_n1.loc[:,~results_n1.columns.str.contains('_DROP', case=False)]
+
+# We repeat with max_date results back to results_n1
+results_n1 = results_n1.merge(max_date_n1,how='left',left_on=['Artist','Title'], right_on = ['Artist','Title'],suffixes = ('', '_DROP'))
+results_n1 = results_n1.loc[:,~results_n1.columns.str.contains('_DROP', case=False)]
+
+# We calculate the Delta: the diff between the first and last times a song reached number1
+results_n1['DeltaTimeN1'] = (results_n1['LastDateN1'] - results_n1['FirstDateN1']) / np.timedelta64(1, 'D')
+# We create a dataframe with the results
+results_n1_agg = results_n1.groupby(['Artist','Title','FirstDateN1','LastDateN1'])['DeltaTimeN1'].max().reset_index()
+qty_n1_artists = results_n1_agg['Artist'].nunique()
+qty_n1_songs = results_n1[['TitleArtist']].nunique()
+mean_n1_songs_per_artist = qty_n1_songs / qty_n1_artists
+start_parsing = results_n1['FirstDateN1'].min()
+end_parsing = results_n1['LastDateN1'].max()
+mean_delta = results_n1['DeltaTimeN1'].mean()
+min_delta = results_n1['DeltaTimeN1'].min()
+max_delta = results_n1['DeltaTimeN1'].max()
+n1_header_data = [qty_n1_artists,qty_n1_songs,mean_n1_songs_per_artist,start_parsing,end_parsing,mean_delta,min_delta,max_delta]
+list_columns = ['qty_n1_artists','qty_n1_songs','avg_n1_songs_per_artist','start_parsing','end_parsing','mean_delta','min_delta','max_delta']
+# We create a cdf graph for the overall distribution of the songs
+header_n1_songs = pd.DataFrame(data = [n1_header_data], columns = list_columns)
+header_n1_songs.columns = list_columns
+results_n1_agg['cumsum_songs'] = results_n1_agg.reset_index().index
+all_songs = len(results_n1_agg.index)
+results_n1_agg['cdf'] = results_n1_agg['cumsum_songs']*100 / all_songs
+
+try: 
+    fp.density_function_plot_n1_songs(current_dir,header_n1_songs,results_n1_agg)
+except Exception as e:
+    print('Error trying to create a cdf graph for n1 songs')
+    print(e)
+    logger.debug(f'{e}')
 
         
 
